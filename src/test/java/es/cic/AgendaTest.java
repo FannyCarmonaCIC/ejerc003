72a42f7919943e31437382cbd15dfbd2b84655f4
Fanny <fcarmonaCIC@outlook.es>
2023-07-13 19:38:21 +0200

    Cambios menores

---
+++ src/test/java/es/cic/AgendaTest.java
@@ -0,0 +1,19 @@
+package es.cic;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class AgendaTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
